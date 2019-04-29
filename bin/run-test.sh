ebf082c30a8f25f55ac8e8740833fa06ddd4b876
Garren Smith <garren.smith@gmail.com>
2019-04-29 10:21:58 +0200

    Update travis
    Update travis file to:
    
    * Use latest selenium with firefox.
    * Node 8
    * Run CouchDB in docker like pouchdb

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+export COUCH_HOST='http://127.0.0.1:5984'
+
+if [ ! -z $TRAVIS ]; then
+  source ./bin/run-couchdb-on-travis.sh
+fi
+
+printf 'Waiting for host to start .'
+WAITING=0
+until $(curl --output /dev/null --silent --head --fail --max-time 2 $COUCH_HOST); do
+    if [ $WAITING -eq 4 ]; then
+        printf '\nHost failed to start\n'
+        exit 1
+    fi
+    let WAITING=WAITING+1
+    printf '.'
+    sleep 5
+done
+printf '\nHost started :)'
+
 : ${CLIENT:="node"}
 
 if [ "$CLIENT" == "node" ]; then
