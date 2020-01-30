44405b5ef17883a8b6def7eea725bb599330a658
Joren l'Ami <joren@distinct.nl>
2020-01-30 16:56:23 +0100

    nyc coverage

--- webpack.test.node.config.js
+++ webpack.test.node.config.js
@@ -35,7 +35,16 @@ let nodeTarget = {
             ],
           },
         }
-      }
+      },
+      {
+        test: /\.js$|\.jsx$/,
+        use: {
+          loader: 'istanbul-instrumenter-loader',
+          options: { esModules: true }
+        },
+        enforce: 'post',
+        exclude: /node_modules|\.spec\.js$/,
+      },
     ]
   },
 };
